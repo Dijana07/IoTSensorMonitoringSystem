44c0e2085e2c55954d51ad3da7ab962c74c6c2f8
Dijana Stojkovic <dijana.stojkovic83@gmail.com>
2026-06-14 12:07:51 +0200

    add shared library

---
+++ IoT.Shared/Models/SensorTelemetry.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IoT.Shared
+{
+	public class SensorTelemetry
+	{
+		public Guid Id
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+			set
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public DateTime DateTime
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+			set
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public double Value
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+			set
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public SensorStatus Status
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+			set
+			{
+				throw new NotImplementedException();
+			}
+		}
+	}
+}
